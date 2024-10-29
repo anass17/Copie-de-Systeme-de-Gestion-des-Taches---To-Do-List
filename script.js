25c8e2126f47a48c4b59805cc322c9512ac4cf79
anass17 <anassboutaib2018@gmail.com>
2024-10-30 00:35:59 +0100

    Added: add tasks modal

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-// const addTaskBtn = document.querySelector('#add-task');
-// const taskAddingModal = document.querySelector('#task-adding-modal');
+const addTaskBtn = document.querySelector('#add-task');
+const taskAddingModal = document.querySelector('#tasks-adding-modal');
+const closeModalBtn = document.querySelector('#close-modal');
 
-// addTaskBtn.addEventListener('click', function () {
-//     taskAddingModal.classList.remove('hidden');
-// });
+addTaskBtn.addEventListener('click', function () {
+    taskAddingModal.classList.remove('hidden');
+    taskAddingModal.classList.add('flex');
+});
+
+closeModalBtn.addEventListener('click', function () {
+    taskAddingModal.classList.add('hidden');
+    taskAddingModal.classList.remove('flex');
+});
