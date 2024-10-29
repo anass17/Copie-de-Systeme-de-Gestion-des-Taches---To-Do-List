f075d724b7ac4fd67ff58d99179f8be1a3d54ee7
anass17 <anassboutaib2018@gmail.com>
2024-10-29 16:02:28 +0100

    Coded ToDo list in HTML with Tailwind

---
+++ script.js
@@ -0,0 +1,6 @@
+// const addTaskBtn = document.querySelector('#add-task');
+// const taskAddingModal = document.querySelector('#task-adding-modal');
+
+// addTaskBtn.addEventListener('click', function () {
+//     taskAddingModal.classList.remove('hidden');
+// });
